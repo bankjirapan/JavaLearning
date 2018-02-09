a6c9302449f56d3d483e0b8a3294c90e06cb0c1d
BANKCOM <jirapan_yankhan@hotmail.com>
2018-02-09 22:19:57 +0700

    เพิ่มไฟล์ TestStudent

---
+++ Agggretion/src/student/view/testStudent.java
@@ -0,0 +1,22 @@
+
+package student.view;
+
+/**
+ *
+ * @author bankcom
+ */
+
+//นำไฟล์เข้ามา 
+import student.model.Address;
+public class testStudent {
+    
+    public static void main(String[] args) {
+        
+        Address bankAddrer = new Address("99","3","Prachautid","Bangkok","10140");
+        
+        
+        
+        
+    }
+    
+}
