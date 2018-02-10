0241948aae98c5e06d684ebf7eaa0ba68dd823b2
BANKCOM <jirapan_yankhan@hotmail.com>
2018-02-10 22:08:12 +0700

    Upload BMI

--- ArrayBMI/src/arraybmi/ArrayBMI.java
+++ ArrayBMI/src/arraybmi/ArrayBMI.java
@@ -1,4 +1,3 @@
-
 package arraybmi;
 
 /**
@@ -7,13 +6,41 @@ package arraybmi;
  */
 public class ArrayBMI {
 
-    /**≠≠
+    /**
+     * ≠≠
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
-        // TODO code application logic here
+
+        BMI Bank = new BMI("BANK", 47, 165);
+
+        System.out.println(Bank);
+
     }
-    
+
+    public static String[] bmiStudent(BMI bmi[]) {
+        String[] NumofStudent = new String[bmi.length];
+
+        for (int i = 0; i < NumofStudent.length; i++) {
+            NumofStudent[i] = BMI_Cat(bmi[i]);
+        }
+        return NumofStudent;
+    }
+
+    public static String BMI_Cat(BMI bmi) {
+        String bmiCategory;
+        if (bmi.getCalBMI() < 0) {
+            bmiCategory = "none";
+        } else if (bmi.getCalBMI() < 18.5) {
+            bmiCategory = "under weight";
+        } else if (bmi.getCalBMI() < 25) {
+            bmiCategory = "normal weight";
+        } else if (bmi.getCalBMI() < 30) {
+            bmiCategory = "over weight";
+        } else {
+            bmiCategory = "obesity";
+        }
+        return bmiCategory;
+    }
 }
