fcd7bd5241bd698dbfd0712e5a143c5f0e647432
BANKCOM <jirapan_yankhan@hotmail.com>
2018-02-10 21:09:19 +0700

    สร้างโปรเจค ArrayBMI

---
+++ ArrayBMI/src/arraybmi/ArrayBMI.java
@@ -0,0 +1,19 @@
+
+package arraybmi;
+
+/**
+ *
+ * @author bankcom
+ */
+public class ArrayBMI {
+
+    /**≠≠
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        // TODO code application logic here
+    }
+    
+}
