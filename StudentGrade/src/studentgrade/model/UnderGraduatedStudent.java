97131934e3cb26b6d1c5e6892f1ae7a1a03cfa9a
INT105 <jirapan_yankhan@hotmail.com>
2018-02-12 09:05:41 +0700

    Upload project Student

---
+++ StudentGrade/src/studentgrade/model/UnderGraduatedStudent.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentgrade.model;
+
+/**
+ *
+ * @author INT105
+ */
+public class UnderGraduatedStudent extends Student{
+    
+    private String project;
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    @Override
+    public String toString() {
+        return "UnderGraduatedStudent{" + "project=" + project + '}';
+    }
+    
+    
+    
+}
