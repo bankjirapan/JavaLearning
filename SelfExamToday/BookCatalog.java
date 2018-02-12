911830f90f2649bd03c4546ff9038c4312288703
llÚJÓ <jirapan_yankhan@hotmail.com>
2018-02-12 11:46:13 +0700

    ๊Update Comment

--- SelfExamToday/BookCatalog.java
+++ SelfExamToday/BookCatalog.java
@@ -25,37 +25,37 @@ public class BookCatalog {
     public int addBook(Book bookAdded) {
 
         int value = 0; // กำหนค่าแรกให้เท่ากับ 0
-        for (int i = 0; i < books.length; i++) { // วนลูบน้อยกว่าความยาวของ book
-            if (books[i] == null) { // ถ้า book ช่องที่ i มันเท่ากับ null  
-                books[i] = bookAdded; //เพิ่มหนังสือเข้าไป
-                this.numOfBooks++; //เพิ่มหนังเข้าไปแล้วบวกค่า
-                value = i; // กำหนดว่าความยาวที่เพิ่มไปเท่าไหร่
-                break; //หยุด
+        for (int i = 0; i < books.length; i++) {         // วนลูบน้อยกว่าความยาวของ book
+            if (books[i] == null) {                      // ถ้า book ช่องที่ i มันเท่ากับ null  
+                books[i] = bookAdded;                    //เพิ่มหนังสือเข้าไป
+                this.numOfBooks++;                       //เพิ่มหนังเข้าไปแล้วบวกค่า
+                value = i;                               // กำหนดว่าความยาวที่เพิ่มไปเท่าไหร่
+                break;                                   //หยุด
 
-            } else { //แต่ถ้ามันเต็มแล้ว
-                value = -1;  // รีเทิน -1
+            } else {                                     //แต่ถ้ามันเต็มแล้ว
+                value = -1;                              // รีเทิน -1
             }
         }
 
-        return value;   //รีเทินค่าของ value = i;
+        return value;                                    //รีเทินค่าของ value = i;
 
     }
 
     public int unAvailableBook(Book bookUnAvailable) {
-    int value = 0;
-        for (int i = 0; i < books.length; i++) {
-            if (books[i].equals(bookUnAvailable)) {
-                books[i] = null;
-                this.numOfBooks++;
-                value = i;
-                break;
+    int value = 0;                                  //กำหนดค่า เท่ากับ 0
+        for (int i = 0; i < books.length; i++) {    //วนลูปความยาวน้อยว่าbook
+            if (books[i].equals(bookUnAvailable)) { // เช็คว่า book ช่องที่ i มันเท่ากับ parameter ที่รับเข้ามาล่าว
+                books[i] = null;                    //เซ็ค Book ช่องที่ i มันเท่ากับ null;
+                this.numOfBooks++;                  //หนังสือที่ลบ มันมีเท่านี้นะ ให้บวกค่าไป
+                value = i;                          // มันลบไปเท่าไหร่นับ loop 
+                break;                              // หยุดการทำงาน
 
             } else {
-                value = -1;
+                value = -1;                         // ไม่เท่ากัน รีเทิน -1
             }
         }
 
-        return value;
+        return value;                               //รีเทินค่าของ value = i;
 
 
     }
