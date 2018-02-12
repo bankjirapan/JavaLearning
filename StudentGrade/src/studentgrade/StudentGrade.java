1bc9a9d5c111f4a6071e56c3d217b22c82566875
INT105 <jirapan_yankhan@hotmail.com>
2018-02-12 09:31:51 +0700

    ทดสอบ OOP

--- StudentGrade/src/studentgrade/StudentGrade.java
+++ StudentGrade/src/studentgrade/StudentGrade.java
@@ -5,6 +5,8 @@
  */
 package studentgrade;
 
+import studentgrade.model.UnderGraduatedStudent;
+
 /**
  *
  * @author INT105
@@ -15,7 +17,11 @@ public class StudentGrade {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        UnderGraduatedStudent bank = new UnderGraduatedStudent();
+        
+        System.out.println(bank);
+        
     }
     
 }
