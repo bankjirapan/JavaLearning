ad33bfdac9a5b96800533a15db414aa1d759e808
INT105 <Jirapan_yankhan@hotmail.com>
2018-02-19 10:52:10 +0700

    [ UPDATE ] Add coments

--- AbstractLearning/src/abstractlearning/model/Square.java
+++ AbstractLearning/src/abstractlearning/model/Square.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package abstractlearning.model;
 
-/**
- *
- * @author INT105
- */
+
+//เรียกใช้ Class interface จะผ่าน implements
 public abstract class Square implements ShapeInterface{
 
     private double side;
