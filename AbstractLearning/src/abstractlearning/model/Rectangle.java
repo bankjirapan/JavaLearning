403fce9fe3d9293533e84e6fa0cf3b4eb8705853
INT105 <Jirapan_yankhan@hotmail.com>
2018-02-19 11:03:46 +0700

    [ UPDATE ] Add implement Class

--- AbstractLearning/src/abstractlearning/model/Rectangle.java
+++ AbstractLearning/src/abstractlearning/model/Rectangle.java
@@ -1,28 +1,30 @@
-
 package abstractlearning.model;
 
-
 // Class โดดเดี่ยวไม่ผูกกับใคร
-
 //สร้างได้อิสระ ไม่ผูกกับโครงสร้างใดเลย จะสร้างอะไรก็ได้
-
-
 //ถ้าโยงไป implements จะต้องมี method ของ implement ใช้งานด้วย
-public class Rectangle {
-    
+public class Rectangle implements ShapeInterface {
+
     private double weight;
     private double lenght;
 
-   
     public Rectangle(double weight, double lenght) {
         this.weight = weight;
         this.lenght = lenght;
     }
-    
-    public double computeArea(){
-        return weight*lenght;
+
+    // Class นี้มาจาก Implement ที่ต้องมี
+    public double findArea() {
+        return weight * lenght;
     }
-    
-    
-    
+    // Class นี้มาจาก Implement ที่ต้องมี
+
+    public double findCircumference() {
+        return 2 * (weight + lenght);
+    }
+
+    public double computeArea() {
+        return weight * lenght;
+    }
+
 }
