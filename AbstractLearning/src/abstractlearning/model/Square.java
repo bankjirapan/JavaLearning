24fb276edfd4f0a44055cfb748b49fce092e617a
INT105 <Jirapan_yankhan@hotmail.com>
2018-02-19 10:43:12 +0700

    [ UPDATE ] interface abstract

--- AbstractLearning/src/abstractlearning/model/Square.java
+++ AbstractLearning/src/abstractlearning/model/Square.java
@@ -9,6 +9,33 @@ package abstractlearning.model;
  *
  * @author INT105
  */
-public class Square {
+public abstract class Square implements ShapeInterface{
+
+    private double side;
+
+    public Square() {
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+     public  double findArea(){
+         return side*side;
+     }
+
+    @Override
+    public String toString() {
+        return "Square{" + "side=" + side + '}';
+    }
+    
+    
     
 }
