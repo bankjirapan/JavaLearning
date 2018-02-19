90a19a371a0c94be6a4c55ef5dddc5a3b4cf2ce8
INT105 <Jirapan_yankhan@hotmail.com>
2018-02-19 10:56:32 +0700

    [ UPDATE ] Add comment

--- AbstractLearning/src/abstractlearning/model/ShapeInterface.java
+++ AbstractLearning/src/abstractlearning/model/ShapeInterface.java
@@ -1,18 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package abstractlearning.model;
 
-/**
- *
- * @author INT105
- */
+
+//ประกาศว่าเป็น interface
 public interface ShapeInterface {
     
     int color = 256;
     
+    
+    //เป็น Method 
     public abstract double findArea();
     public abstract double findCircumference();
     
