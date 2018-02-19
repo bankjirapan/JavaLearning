a7c7d45338f674bcf141108dd886ef071e6169f6
INT105 <Jirapan_yankhan@hotmail.com>
2018-02-19 11:00:50 +0700

    [ ADD ] Coding

--- AbstractLearning/src/abstractlearning/model/Rectangle.java
+++ AbstractLearning/src/abstractlearning/model/Rectangle.java
@@ -3,6 +3,26 @@ package abstractlearning.model;
 
 
 // Class โดดเดี่ยวไม่ผูกกับใคร
+
+//สร้างได้อิสระ ไม่ผูกกับโครงสร้างใดเลย จะสร้างอะไรก็ได้
+
+
+//ถ้าโยงไป implements จะต้องมี method ของ implement ใช้งานด้วย
 public class Rectangle {
+    
+    private double weight;
+    private double lenght;
+
+   
+    public Rectangle(double weight, double lenght) {
+        this.weight = weight;
+        this.lenght = lenght;
+    }
+    
+    public double computeArea(){
+        return weight*lenght;
+    }
+    
+    
     
 }
