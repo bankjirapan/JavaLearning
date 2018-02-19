0ae36023dafba9a474895b081ac545104fb2128e
INT105 <Jirapan_yankhan@hotmail.com>
2018-02-19 11:31:00 +0700

    [ UPDATE ] Add Compareto

--- AbstractLearning/src/abstractlearning/model/Rectangle.java
+++ AbstractLearning/src/abstractlearning/model/Rectangle.java
@@ -10,11 +10,17 @@ package abstractlearning.model;
 
 
 //ถ้าโยงไป implements จะต้องมี method ของ implement ใช้งานด้วย
-public class Rectangle implements ShapeInterface {
+public class Rectangle implements ShapeInterface , Comparable<Rectangle> {
 
     private double weight;
     private double lenght;
 
+    
+    //มันมาจาก implement ไอ้  Compareto อะ
+    public int compareTo(Rectangle  o){   
+        return (int)(findArea()-o.findArea());  
+    }
+    
     public Rectangle(double weight, double lenght) {
         this.weight = weight;
         this.lenght = lenght;
@@ -24,8 +30,8 @@ public class Rectangle implements ShapeInterface {
     public double findArea() {
         return weight * lenght;
     }
+    
     // Class นี้มาจาก Implement ที่ต้องมี
-
     public double findCircumference() {
         return 2 * (weight + lenght);
     }
