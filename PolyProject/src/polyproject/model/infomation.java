9e854b6788f6f83ae9484008a5f873f5fdfac5ca
INT105 <Jirapan_yankhan@hotmail.com>
2018-04-02 08:37:31 +0700

    update

---
+++ PolyProject/src/polyproject/model/infomation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polyproject.model;
+
+/**
+ *
+ * @author INT105
+ */
+public interface infomation {
+    
+    //การตั้งกฏ ว่ามันต้องมีโครงสร้างนี้
+    
+    public boolean isLife();
+    public String giveDetail();
+    public String move();
+    
+    
+}
