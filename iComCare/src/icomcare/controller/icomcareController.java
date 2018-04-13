8849addc40021a09f91f567dd9b3e83fc89f982d
BANKCOM <jirapan_yankhan@hotmail.com>
2018-04-13 10:16:32 +0700

    add files controller

---
+++ iComCare/src/icomcare/controller/icomcareController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package icomcare.controller;
+
+import icomcare.database.ConnectManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author bankcom
+ */
+public class icomcareController {
+
+    private Connection connect;
+
+    private String UsernameDB = "bank";
+    private String PasswordDB = "bank";
+    private String LocationDB = "jdbc:derby://localhost:1527/icomcare";
+
+    public icomcareController() throws ClassNotFoundException, SQLException {
+
+        connect = ConnectManager.ConnectDatabase(
+                LocationDB,
+                UsernameDB,
+                PasswordDB
+        );
+        
+        //System.out.println("เชื่อมต่อฐานข้อมูลสำเร็จ");
+
+    }
+
+}
