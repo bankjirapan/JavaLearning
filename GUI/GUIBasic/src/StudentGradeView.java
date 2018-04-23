3bf84a2d30a1dabc3a9436122a3ca5c8cdf3d3cf
INT105 <Jirapan_yankhan@hotmail.com>
2018-04-23 10:49:08 +0700

    create view

---
+++ GUI/GUIBasic/src/StudentGradeView.java
@@ -0,0 +1,26 @@
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author INT105
+ */
+public class StudentGradeView {
+    
+    private JFrame freameGrade;
+    private JLabel lblSubject[];
+    private JTextField tetGrade[];
+    private JTextField txtCredit[];
+    private JButton btnCalulate;
+    
+    
+}
