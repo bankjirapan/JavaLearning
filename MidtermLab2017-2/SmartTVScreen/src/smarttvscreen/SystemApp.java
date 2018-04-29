6c053a8265e7d80043730a5cbdd99d68ad69742d
BANKCOM <jirapan_yankhan@hotmail.com>
2018-04-29 22:03:25 +0700

    Edit Files

--- MidtermLab2017-2/SmartTVScreen/src/smarttvscreen/SystemApp.java
+++ MidtermLab2017-2/SmartTVScreen/src/smarttvscreen/SystemApp.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package smarttvscreen;
 
 public class SystemApp extends Application{
@@ -20,26 +20,3 @@ public class SystemApp extends Application{
         return this.getName() + "\n - SystemApp " + "type: " + type + "\n" + this.getDescription();
     }
 }
-=======
-package smarttvscreen;
-
-public class SystemApp {
-
-    private String type;
-
-    public SystemApp(String name, String type) {
-
-        this.type = type;
-    }
-
-    public SystemApp(String name, String description, String type) {
-
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return this.getName() + "\n - SystemApp " + "type: " + type + "\n" + this.getDescription();
-    }
-}
->>>>>>> d55087d320aade705a735a719d7db7e95b19bf96
