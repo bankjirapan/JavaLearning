d0b62c1dcd24184fba1d52779036f1a4de5561a7
BANKCOM <jirapan_yankhan@hotmail.com>
2018-04-29 21:40:04 +0700

    เพิ่ม Comments

--- MidtermLab2017-2/SmartPhone/src/smartphone/Folder.java
+++ MidtermLab2017-2/SmartPhone/src/smartphone/Folder.java
@@ -3,33 +3,51 @@ package smartphone;
 public class Folder extends Item {
 
     private static int maxApp = 4;
+    
+    //Load Class Dependency ชื่แว่า apps
     private Application[] apps;
     private int appCount;
 
     public Folder(String name) {
 
         super(name);
+        
+        //ตั้งค่า apps โดยมีขนาด Array เท่่กับ MaxApp;
         this.apps = new Application[maxApp];
+        
+        //ค่าเริ่มต้นของ AppCount
         appCount = 0;
 
     }
 
+    
+    //Method Return ค่าเป็น True
     public boolean addItem(Application app) {
 
+        //ถ้า Appcount ยังน้อยกว่า Maxapp 
         if (appCount < maxApp) {
 
+            //วนลูปที่ AppCount
             for (int i = 0; i < appCount; i++) {
 
+                //ถ้ามันมีข้อมูลอยู่แล้วนั่นคือ Check เท่ากับ app ที่รับเข้ามา
                 if (apps[i].equals(app)) {
+                    
+                    //รีเทิน false;
                     return false;
                 }
-
             }
+            //แสดงว่าช่องนั้นมันว่างให้เอาบอกว่า พารามิเตอร์ด้านบนยังมีช่อง appcount เหลืออยู่ให้นำข้อมูล app ไปใส่
             this.apps[appCount] = app;
+            //เพิ่มค่าของ appCpunt กรณีเพิ่มข้อมูลเสร็จแล้ว
             this.appCount++;
+            
+            //Return ค่า
             return true;
 
+            // ถ้ามันมากกว่าแล้ว Return false
         } else {
+            
             return false;
         }
 
