e64ca88430a01c5f68f9b473b56864222835ceca
INT105 <Jirapan_yankhan@hotmail.com>
2018-04-30 09:49:08 +0700

    add files calulate

---
+++ GUI/GUIBasic/src/Controller/Calulate.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author INT105
+ */
+public class Calulate implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
+    }
+    
+}
