395b7117bb9d509fb64ecc131fbe78cc007e00f2
INT105 <Jirapan_yankhan@hotmail.com>
2018-04-30 11:19:12 +0700

    upload files

---
+++ GUI/StudentDBProject/src/sit/view/TestStudent.java
@@ -0,0 +1,19 @@
+package sit.view;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import sit.controller.StudentController;
+import sit.model.Student;
+
+public class TestStudent {
+
+    public static void main(String[] args) {
+      
+    }
+}
+
