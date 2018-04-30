a70b39621d0f744843aebe9f40685a9cd8e8462f
INT105 <Jirapan_yankhan@hotmail.com>
2018-04-30 09:56:03 +0700

    add

--- GUI/GUIBasic/src/Controller/Calulate.java
+++ GUI/GUIBasic/src/Controller/Calulate.java
@@ -7,16 +7,31 @@ package Controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JTextField;
 
 /**
  *
  * @author INT105
  */
-public class Calulate implements ActionListener{
+public class Calulate implements ActionListener {
+
+    private JTextField txtGrade[];
+    private JTextField txtCredit[];
+    private JTextField txtGPA;
+
+    public Calulate(JTextField[] txtGrade, JTextField[] txtCredit, JTextField txtGPA) {
+        this.txtGrade = txtGrade;
+        this.txtCredit = txtCredit;
+        this.txtGPA = txtGPA;
+    }
+    
+    
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println(e.getActionCommand());
+
+        
+        //System.out.println(e.getActionCommand());
     }
-    
+
 }
