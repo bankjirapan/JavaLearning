cdf8a5b66bff18f249f7d17c556faf302e3772c5
INT105 <Jirapan_yankhan@hotmail.com>
2018-05-07 09:55:58 +0700

    update

--- MySelfLearning/University/src/university/University.java
+++ MySelfLearning/University/src/university/University.java
@@ -6,6 +6,7 @@
 package university;
 
 import university.model.KinderGartenStudent;
+import university.model.Student;
 import university.model.UniversityStudent;
 
 /**
@@ -18,10 +19,17 @@ public class University {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        KinderGartenStudent child1 = new KinderGartenStudent(10000, 50000, "Deb", 60123, "DebKid1",
+        Student child1 = new KinderGartenStudent(10000, 50000, "Deb", 60123, "DebKid1",
                 25000);
-        UniversityStudent child2 = new UniversityStudent(10000, true, true, 60124,
+        Student child2 = new UniversityStudent(10000, true, true, 60124,
                 "DebKid2", 50000);
+
+        //สามารถใช้แบบนี้ก็ได้นะ
+        KinderGartenStudent child3 = new KinderGartenStudent(10000, 50000, "Deb", 60123, "DebKid1",
+                25000);
+        UniversityStudent child4 = new UniversityStudent(10000, true, true, 60124,
+                "DebKid2", 50000);
+
         System.out.println(child1);
         System.out.println(child2);
         System.out.println();
