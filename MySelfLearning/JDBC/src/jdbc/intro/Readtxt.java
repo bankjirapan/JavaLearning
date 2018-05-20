01bbdf0ac26ee964d01d5fcdff822212b9567b4f
BANKCOM <jirapan_yankhan@hotmail.com>
2018-05-20 11:12:17 +0700

    None Finish Upload to Edit

---
+++ MySelfLearning/JDBC/src/jdbc/intro/Readtxt.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbc.intro;
+
+/**
+ *
+ * @author bankcom
+ */
+public class Readtxt {
+    
+    public static void main(String[] args) {
+        
+        String input = "1,2,3,4,a";
+        
+       
+        
+        
+    }
+    
+}
