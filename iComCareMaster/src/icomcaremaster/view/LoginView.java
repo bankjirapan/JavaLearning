154f7f30c509150aa74efa8f440e69700fdab157
BANKCOM <jirapan_yankhan@hotmail.com>
2018-05-22 19:08:03 +0700

    add label to center

--- iComCareMaster/src/icomcaremaster/view/LoginView.java
+++ iComCareMaster/src/icomcaremaster/view/LoginView.java
@@ -9,6 +9,7 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 /**
  *
  * @author bankcom
@@ -24,26 +25,19 @@ public class LoginView {
     public static void MainFrame() {
 
         JFrame Login = new JFrame();
-        
+
         JLabel WelcomeLogin = new JLabel("Welcome to iComCare");
-        
-       
+
         //สร้าง Panel
         JPanel MainPanel = new JPanel();
-        //ตั้งค่า Panel
-        MainPanel.setLayout(new BorderLayout());
-        
-        MainPanel.add(WelcomeLogin,BorderLayout.NORTH);
-        
-        
-        
-        Login.add(MainPanel);
-        
-        
-       
-        
-       
+   
 
+        //ตั้งค่า Panel
+        MainPanel.add(WelcomeLogin);
+        
+        
+
+        Login.add(MainPanel);
         //New Frame 
         Login.setSize(500, 500);
         Login.setLocationRelativeTo(Login);
