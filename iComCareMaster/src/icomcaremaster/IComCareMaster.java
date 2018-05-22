e246a1eb536e2c143a76ea9c8a5d5386e2492c33
BANKCOM <jirapan_yankhan@hotmail.com>
2018-05-22 18:51:09 +0700

    upload icomcare master

---
+++ iComCareMaster/src/icomcaremaster/IComCareMaster.java
@@ -0,0 +1,21 @@
+
+package icomcaremaster;
+
+import icomcaremaster.view.LoginView;
+
+/**
+ *
+ * @author bankcom
+ */
+public class IComCareMaster {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        LoginView login = new LoginView();
+    }
+    
+}
---
+++ iComCareMaster/src/icomcaremaster/view/LoginView.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package icomcaremaster.view;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+/**
+ *
+ * @author bankcom
+ */
+public class LoginView {
+
+    public LoginView() {
+
+        MainFrame();
+
+    }
+
+    public static void MainFrame() {
+
+        JFrame Login = new JFrame();
+        
+        JLabel WelcomeLogin = new JLabel("Welcome to iComCare");
+        
+       
+        //สร้าง Panel
+        JPanel MainPanel = new JPanel();
+        //ตั้งค่า Panel
+        MainPanel.setLayout(new BorderLayout());
+        
+        MainPanel.add(WelcomeLogin,BorderLayout.NORTH);
+        
+        
+        
+        Login.add(MainPanel);
+        
+        
+       
+        
+       
+
+        //New Frame 
+        Login.setSize(500, 500);
+        Login.setLocationRelativeTo(Login);
+        Login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Login.setVisible(true);
+
+    }
+
+}
