6d455d4712f1fef15bd1bad9a00aa6b6fa65df44
BANKCOM <jirapan_yankhan@hotmail.com>
2018-05-22 22:40:19 +0700

    click event success

--- iComCareMaster/src/icomcaremaster/IComCareMaster.java
+++ iComCareMaster/src/icomcaremaster/IComCareMaster.java
@@ -2,8 +2,8 @@ package icomcaremaster;
 
 import icomcaremaster.controller.MainController;
 import icomcaremaster.view.LoginView;
+import icomcaremaster.view.RegisterView;
 import java.sql.SQLException;
-
 
 /**
  *
@@ -14,20 +14,19 @@ public class IComCareMaster {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException, ClassNotFoundException{
-       
-        
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
         MainController main = new MainController();
-        
-        if(main.Status() != 1){
+
+        if (main.Status() != 1) {
             System.out.println("Error");
         } else {
             System.out.println("OK");
         }
-         // LoginView login = new LoginView();
-         
-         
-         
+        // LoginView login = new LoginView();
+
+        RegisterView regis = new RegisterView();
+
     }
 
 }
